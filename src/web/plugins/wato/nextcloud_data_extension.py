915e8b4c0c51afd5382bceffb3cef9f382e15b42
Martin Hasin <martin.hasin@gmail.com>
2022-07-08 10:00:57 +0200

    Add hostname variable
    You can set hostname fom rules

--- src/web/plugins/wato/nextcloud_data_extension.py
+++ src/web/plugins/wato/nextcloud_data_extension.py
@@ -8,6 +8,27 @@
 # email: martin.hasin@gmail.com
 #
 #
+from cmk.gui.i18n import _
+from cmk.gui.valuespec import (
+    CascadingDropdown,
+    Dictionary,
+    Integer,
+    ListOf,
+    ListOfStrings,
+    RegExp,
+    TextAscii,
+    Tuple,
+)
+
+from cmk.gui.plugins.wato import (
+    rulespec_registry,
+    HostRulespec,
+    SNMPCredentials,
+)
+
+from cmk.gui.plugins.wato.active_checks import (
+    RulespecGroupIntegrateOtherServices,
+)
 
 group = "datasource_programs"
 register_rule(group,
@@ -21,13 +42,30 @@ register_rule(group,
                                         allow_empty=False,
                                         )
                    ),
+            #      ("password",
+            #         IndividualOrStoredPassword(
+            #            title=_("Password"),
+            #            allow_empty=False,)
+            #      ),
+
+
+                  ("hostnamenextcloud",
+                     TextAscii(
+                        title = _("DNS Hostname or IP address"),
+                           default_value = "$HOSTNAME$",
+                           allow_empty = True,
+                           help = _("You can specify a hostname or IP address different from IP address "
+                                    "of the host as configured in your host properties."),
+                   )),
+
+
                   ("no_cert_check", FixedValue(
                           True,
                           title=_("Disable SSL certificate validation"),
                           totext=_("SSL certificate validation is disabled"),
                               )
                    )],
-                      optional_keys=['no_cert_check']
+                      optional_keys=['no_cert_check','hostnamenextcloud']
                         ),
               title=_("Check health state of nextcloud "),
               help=_("This rule selects the ownloud agent instead of the "
